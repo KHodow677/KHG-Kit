6fd011826efbd89c483a0f23cb55bbb4a56f212d
KHodow677 <kylehodowany@utexas.edu>
2024-07-29 20:55:59 -0500

    Finished GFX Refactor

--- engine/external/gfx/include/gfx/renderer.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void set_projection_matrix(void);
-void renderer_init(void);
-void renderer_flush(void);
-void renderer_begin(void);
--- engine/external/gfx/include/gfx/shader.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "gfx/gfx.h"
-
-uint32_t shader_create(GLenum type, const char *src);
-LfShader shader_prg_create(const char* vert_src, const char *frag_src);
-void shader_set_mat(LfShader prg, const char* name, mat4 mat); 
-void set_projection_matrix(void);
