cd4298830add3362214b925ce44d09a3d1c98b03
Kyle Hodowany <kylehodowany@utexas.edu>
2024-08-07 11:39:28 -0500

    Added Tiny Encryption Algo Wrapper

---
+++ engine/khg_tea/src/file_handler.c
@@ -0,0 +1,59 @@
+#include "khg_tea/file_handler.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int tea_operate(tea_operation prm) {
+  int status;
+  char output_filename[MAX_FILENAME_LENGTH + 2];
+  int ed_status, enc_dec_mode = 0, i;
+  if (status != ERR_NONE || prm.mode == UNSET || prm.key[0] == 0) {
+    exit(ERR_INVALID_ARG);
+  }
+  for(i = 0; i < prm.count; i++) {
+      if (prm.mode == ENCRYPT) {
+        output_filename[0] = '\0';
+        strcat(output_filename, prm.files[i]);
+        strcat(output_filename, ENCRYPTED_FILE_EXTENSION);
+      }
+      else{
+        if (tea_strip_extension(prm.files[i], ENCRYPTED_FILE_EXTENSION, output_filename) == ERR_INVALID_ARG) {
+          fprintf(stderr,"Warning: Invalid file extension in %s\n", prm.files[i]);
+          continue;
+        }
+        if (prm.flags & FLAG_OUTPUT_TO_STDOUT) {
+          strcpy(output_filename,"(stdout)");
+          enc_dec_mode |= TEA_FLAG_OUTPUT_STDOUT;
+          printf("-------------- %u: %s --------------\n", i+1, prm.files[i]);
+        }
+      }
+      ed_status = tea_encrypt_decrypt (prm.mode, prm.key, enc_dec_mode, prm.files[i], output_filename);
+      if (prm.flags & FLAG_VERBOSE) {
+        printf("%s: %s => %s : %s\n", (prm.mode == ENCRYPT) ? "Encryption" : "Decryption", prm.files[i], output_filename, (ed_status) ? "Success" : "Failed");
+      }
+      if (ed_status == true && prm.flags & FLAG_DELETE_SOURCE_FILE) {
+          printf("Deletion: %s : %s\n", prm.files[i], tea_delete(prm.files[i]) ? "Success": "Failed");
+      }
+  }
+  return ERR_NONE;
+}
+
+bool tea_delete(char *filename) {
+  if (unlink(filename) == -1){
+    fprintf(stderr, "File %s could not be deleted.\n", filename);
+    perror("unlink");
+    return false;
+  }
+  return true;
+}
+
+int tea_strip_extension(char *filename, char *extension, char *out) {
+  int flen = strlen(filename), el = flen - strlen(extension);
+  *out = '\0';
+  if (strcmp (filename + el,extension)) {
+    return ERR_INVALID_ARG;
+  }
+  strcpy(out, filename);
+  *(out + el) = '\0';
+  return ERR_NONE;
+}
---
+++ engine/khg_tea/src/tea.c
@@ -0,0 +1,81 @@
+#include "khg_tea/tea.h"
+#include "khg_utl/error_func.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+
+void tea_decode(uint32_t *v, uint32_t *k) {
+  uint32_t v0 = v[0], v1 = v[1], delta = 0x9e3779b9, n = 32, sum = delta * 32;
+  while(n--) {
+    v1 -= ((v0<<4) + k[2]) ^ (v0 + sum) ^ ((v0>>5) + k[3]);
+    v0 -= ((v1<<4) + k[0]) ^ (v1 + sum) ^ ((v1>>5) + k[1]);
+    sum -= delta;
+  }
+  v[0] = v0;
+  v[1] = v1;
+}
+
+void tea_code(uint32_t *v, uint32_t *k) {
+  uint32_t v0 = v[0], v1 = v[1], delta = 0x9e3779b9, n = 32, sum = 0;
+  while(n--) {
+    sum += delta;
+    v0 += ((v1<<4) + k[0]) ^ (v1 + sum) ^ ((v1>>5) + k[1]);
+    v1 += ((v0<<4) + k[2]) ^ (v0 + sum) ^ ((v0>>5) + k[3]);
+  }
+  v[0] = v0;
+  v[1] = v1;
+}
+
+bool tea_encrypt_decrypt(int mode, char *key, int flags, char *in_file, char *out_file) {
+  char d[DATA_SIZE];
+  int len, inf, outf;
+  outf = (flags & TEA_FLAG_OUTPUT_STDOUT) ? STDOUT_FILENO : 0;
+  if ( outf == 0 && access(out_file, F_OK) == 0) {
+    error_func("Output file already exists", user_defined_data);
+    return false;
+  }
+  if (access(in_file, F_OK) == -1){
+    error_func("Skipping, Input file do not exists", user_defined_data);
+    return false;
+  }
+  if ((inf = open (in_file, O_RDONLY|_O_BINARY)) == -1) {
+    error_func("Open - input", user_defined_data);
+    return false;
+  }
+  if ( outf == 0 ) {
+    if ((outf = open (out_file, O_CREAT|O_WRONLY|_O_BINARY, DEFAULT_FILE_CREATION_MODE)) == -1) {
+      error_func("Open - output", user_defined_data);
+      close(inf);
+      return false;
+    }
+  }
+  while ((len = read (inf,d,DATA_SIZE)) > 0) {
+    memset(&d[len], 0, DATA_SIZE - len);    
+    if (mode == ENCRYPT) {
+      tea_code((uint32_t *) d, (uint32_t *) key);
+    }
+    else {
+      tea_decode((uint32_t *) d, (uint32_t *) key);
+    }
+    if ((len = write (outf,d,DATA_SIZE)) < 0) {
+      error_func("Write", user_defined_data);
+      break;
+    }
+  }
+  if (len < 0){
+    error_func("Read / Write", user_defined_data);
+    goto failed_exit;
+  }
+  close(inf);
+  if (outf != STDOUT_FILENO) {
+    close(outf);
+  }
+  return true;
+  failed_exit:
+    close(inf);
+    if (outf != STDOUT_FILENO) {
+      close(outf);
+    }
+    return false;
+}
