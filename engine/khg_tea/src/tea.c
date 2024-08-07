f7ae4cfdab41d282fb9d9e80a319f3e498f7fd9b
KHodow677 <kylehodowany@utexas.edu>
2024-08-07 15:44:45 -0500

    Tea Linux Fix

--- engine/khg_tea/src/tea.c
+++ engine/khg_tea/src/tea.c
@@ -3,6 +3,18 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#if defined(_WIN32) || defined(_WIN64)
+    #include <io.h>
+    #include <fcntl.h>
+    #define O_BINARY _O_BINARY
+    #define DEFAULT_FILE_CREATION_MODE _S_IREAD | _S_IWRITE
+#else
+    #include <unistd.h>
+    #include <fcntl.h>
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #define O_BINARY 0 // O_BINARY is not needed on POSIX systems
+#endif
 
 void tea_decode(uint32_t *v, uint32_t *k) {
   uint32_t v0 = v[0], v1 = v[1], delta = 0x9e3779b9, n = 32, sum = delta * 32;
@@ -34,12 +46,12 @@ bool tea_encrypt_decrypt(int mode, char *key, int flags, char *in_file, char *ou
     error_func("Skipping, Input file does not exists", user_defined_data);
     return false;
   }
-  if ((inf = open(in_file, O_RDONLY|_O_BINARY)) == -1) {
+  if ((inf = open(in_file, O_RDONLY|O_BINARY)) == -1) {
     error_func("Open - input", user_defined_data);
     return false;
   }
   if ( outf == 0 ) {
-    if ((outf = open (out_file, O_CREAT|O_WRONLY|_O_BINARY, DEFAULT_FILE_CREATION_MODE)) == -1) {
+    if ((outf = open(out_file, O_CREAT|O_WRONLY|O_BINARY, DEFAULT_FILE_CREATION_MODE)) == -1) {
       error_func("Open - output", user_defined_data);
       close(inf);
       return false;
