d289e491f65a4361ebd01aa57a74caf6b43aaa72
KHodow677 <kylehodowany@utexas.edu>
2024-08-27 00:42:37 -0500

    TCP Cleanup

---
+++ engine/khg_tcp/include/khg_tcp/tcp.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <stdbool.h>
+
+#define STCP_STREAM_BUFFER_SIZE 2048
+
+typedef struct tcp_channel tcp_channel;
+typedef struct tcp_server tcp_server;
+
+typedef bool (*stream_output_fn)(const char *buffer, int length, void *user_data);
+
+bool tcp_init(void);
+void tcp_term(void);
+
+tcp_server *tcp_open_server(const char *address, const char *protocol, int max_pending_channels);
+tcp_channel *tcp_accept(tcp_server *server, int timeout_ms);
+void tcp_close_server(tcp_server *server);
+
+tcp_channel *tcp_connect(const char *address, const char *protocol);
+bool tcp_send(tcp_channel *channel, const char *buffer, int length, int timeout_ms);
+int tcp_receive(tcp_channel *channel, char *buffer, int length, int timeout_ms);
+bool tcp_stream_receive(tcp_channel *channel, stream_output_fn stream_output, void *user_data, int timeout_ms);
+
+void tcp_close_channel(tcp_channel *channel);
+
--- src/module_test/tcp_test.c
+++ src/module_test/tcp_test.c
@@ -1,13 +1,14 @@
-#include "khg_tcp/stcp.h"
+#include "khg_tcp/error.h"
+#include "khg_tcp/tcp.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void process_error(stcp_error e, void *user_data) {
-	stcp_channel **channel = (stcp_channel **) user_data;
-	perror(stcp_error_to_string(e));
-	stcp_close_channel(*channel);
-	stcp_terminate();
+void process_error(tcp_error e, void *user_data) {
+	tcp_channel **channel = (tcp_channel **) user_data;
+	perror(tcp_error_to_string(e));
+	tcp_close_channel(*channel);
+	tcp_term();
 	exit(-1);
 }
 
@@ -17,14 +18,14 @@ bool print_buffer(const char *buffer, int length, void *user_data) {
 }
 
 int tcp_test() {
-	stcp_channel *channel = NULL;
-	stcp_set_error_callback(process_error, &channel);
-	stcp_initialize();
+	tcp_channel *channel = NULL;
+	tcp_set_error_callback(process_error, &channel);
+	tcp_init();
 	const char *request = "HEAD / HTTP/1.2\r\n\r\n";
-	channel = stcp_connect("www.google.com", "http");
-	stcp_send(channel, request, strlen(request), 500);
-	stcp_stream_receive(channel, print_buffer, NULL, 500);
-	stcp_close_channel(channel);
-	stcp_terminate();
+	channel = tcp_connect("www.google.com", "http");
+	tcp_send(channel, request, strlen(request), 500);
+	tcp_stream_receive(channel, print_buffer, NULL, 500);
+	tcp_close_channel(channel);
+	tcp_term();
 	return 0;
 }
