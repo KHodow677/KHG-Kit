c381840a8118b0fd881f7f4b44976ae26efc25c0
hodowany <Kyle.Hodowany@caemilusa.com>
2024-08-27 14:29:53 -0500

    Client Server Testing

--- src/module_test/tcp_test.c
+++ src/module_test/tcp_test.c
@@ -5,9 +5,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-#define SERVER "localhost"
-#define PORT "12345"
+char BUFFER[TCP_STREAM_BUFFER_SIZE] = "";
 
 void process_error(tcp_error e, void *user_data) {
 	tcp_channel **channel = (tcp_channel **) user_data;
@@ -19,7 +19,9 @@ void process_error(tcp_error e, void *user_data) {
 
 bool print_buffer(const char *buffer, int length, void *user_data) {
 	(void) user_data;
-	return fwrite(buffer, sizeof(char), length, stdout) == (size_t) length;
+  strcpy(BUFFER, buffer);
+  printf("%s\n", BUFFER);
+	return strlen(buffer) == (size_t) length;
 }
 
 int tcp_test() {
@@ -36,30 +38,41 @@ int tcp_test() {
 }
 
 int tcp_server_test(int argc, char *argv[]) {
+	tcp_channel *channel = NULL;
+	tcp_set_error_callback(process_error, &channel);
   if (streq(argv[1], "host")) {
-    tcp_channel *channel = NULL;
-    tcp_set_error_callback(process_error, &channel);
     tcp_init();
     tcp_server *serv = tcp_open_server("localhost", "https", 1);
     printf("Started Server\n");
     tcp_channel *client = tcp_accept(serv, 5000);
     printf("Connected a Client\n");
-    tcp_send(client, "Hello World!", strlen("Hello World!"), 5000);
+    usleep(16000);
+    tcp_send(client, "Hello World from Server!", 24, 0);
+    printf("Hello World from Server!\n");
+    usleep(16000);
+    tcp_send(client, "Hello World from Server!", 24, 0);
+    printf("Hello World from Server!\n");
+    usleep(16000);
+    tcp_send(client, "Hello World from Server!", 24, 0);
+    printf("Hello World from Server!\n");
+    usleep(16000);
+    tcp_send(client, "exit", 24, 0);
     printf("Sent Message to Client\n");
-    tcp_close_channel(channel);
+    tcp_close_channel(client);
+    tcp_close_server(serv);
+	  tcp_close_channel(channel);
     tcp_term();
     return 0;
   }
   else if (streq(argv[1], "client")) {
-    tcp_channel *channel = NULL;
-    tcp_set_error_callback(process_error, &channel);
     tcp_init();
     tcp_channel *server = tcp_connect("localhost", "https");
     printf("Connected to Server\n");
-    char buffer[TCP_STREAM_BUFFER_SIZE];
-    tcp_receive(server, buffer, strlen("Hello World!"), 5000);
-    printf("Received Message from Server: %s\n", buffer);
-    tcp_close_channel(channel);
+    while (strcmp(BUFFER, "exit") != 0) {
+      tcp_stream_receive(server, print_buffer, NULL, 0);
+    }
+    tcp_close_channel(server);
+	  tcp_close_channel(channel);
     tcp_term();
     return 0;
   }
