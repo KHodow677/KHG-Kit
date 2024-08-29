84f9b2b095468ba4c33f7cf217d3af421f909193
KHodow677 <kylehodowany@utexas.edu>
2024-08-29 02:32:53 -0500

    NodeJS Response HTTP

--- svr/server.js
+++ svr/server.js
@@ -4,12 +4,13 @@ var bodyParser = require('body-parser');
 var serveStatic = require('serve-static');
 var app = connect();
 app.use(serveStatic('app', {
-	'index': ['index.html', 'index.htm', 'default.html']
+    'index': ['index.html', 'index.htm', 'default.html']
 }));
-app.use(bodyParser.urlencoded());
-app.use(function(req, res)	{
-	console.log('Request received.');
-	console.log('Request processed.');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res) {
+    console.log('Request received.');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Request processed.');
 });
 http.createServer(app).listen(80);
 console.log('Server started at http://localhost:80');
