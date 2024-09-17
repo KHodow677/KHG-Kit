c9f37639a4200b4687ca1193749a72007fbb423d
Kyle Hodowany <kylehodowany@utexas.edu>
2024-09-17 14:04:16 -0500

    Fixed CMakeLists.txt

--- src/entity/maps.c
+++ src/entity/maps.c
@@ -12,7 +12,7 @@ void render_map() {
     for (int j = 0; j < map_cols; j++) { 
       int *item = utl_vector_at(*row, j);
       gfx_texture *tex = get_or_add_texture(*item);
-      gfx_image_no_block((j + 0.5f) * GAME_MAP_TILE_SIZE, (i * 0.5f) * GAME_MAP_TILE_SIZE, *tex, 0.0f, 0.0f, CAMERA.position.x, CAMERA.position.y, CAMERA.zoom);
+      gfx_image_no_block((j + 0.5f) * GAME_MAP_TILE_SIZE, (i + 0.5f) * GAME_MAP_TILE_SIZE, *tex, 0.0f, 0.0f, CAMERA.position.x, CAMERA.position.y, CAMERA.zoom);
     }
   }
 }
