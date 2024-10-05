f90477cd2192b8788580507e98e5172fc707535e
KHodow677 <kylehodowany@utexas.edu>
2024-10-05 16:43:32 -0500

    Update

--- src/networking/client.c
+++ src/networking/client.c
@@ -7,50 +7,51 @@
 static int TIMEOUT = 500;
 
 void client_run() {
-    game_client client = {0};
-    tcp_init();
-    client_connect(&client, "165.22.176.143", "http");
-    client_send_message(&client, "Hello Server!");
-    while (true) {
-        client_receive_message(&client);
-    }
-    client_disconnect(&client);
-    tcp_term();
+  game_client client = {0};
+  tcp_init();
+  client_connect(&client, "165.22.176.143", "http");
+  client_send_message(&client, "Hello Server!");
+  while (true) {
+    client_receive_message(&client);
+  }
+  client_disconnect(&client);
+  tcp_term();
 }
 
 void client_connect(game_client *client, const char *ip, const char *port) {
-    printf("Connecting to server at %s:%s\n", ip, port);
-    client->ip = ip;
-    client->port = port;
-    client->server = tcp_connect(ip, port);
-    if (!client->server) {
-        fprintf(stderr, "Failed to connect to server at %s:%s\n", ip, port);
-        exit(EXIT_FAILURE);
-    }
+  printf("Connecting to server at %s:%s\n", ip, port);
+  client->ip = ip;
+  client->port = port;
+  client->server = tcp_connect(ip, port);
+  if (!client->server) {
+    fprintf(stderr, "Failed to connect to server at %s:%s\n", ip, port);
+    exit(EXIT_FAILURE);
+  }
 }
 
 void client_disconnect(game_client *client) {
-    printf("Disconnecting from server\n");
-    tcp_close_channel(client->server);
+  printf("Disconnecting from server\n");
+  tcp_close_channel(client->server);
 }
 
 void client_send_message(game_client *client, const char *message) {
-    const char *request_template = "POST /send HTTP/1.1\r\nHost: %s\r\nContent-Length: %zu\r\n\r\n%s";
-    char formatted_request[1024];
-    snprintf(formatted_request, sizeof(formatted_request), request_template, client->ip, strlen(message), message);
-    
-    if (tcp_send(client->server, formatted_request, strlen(formatted_request), TIMEOUT)) {
-        printf("Message sent: %s\n", message);
-    } else {
-        fprintf(stderr, "Failed to send message\n");
-    }
+  const char *request_template = "POST /send HTTP/1.1\r\nHost: %s\r\nContent-Length: %zu\r\n\r\n%s";
+  char formatted_request[1024];
+  snprintf(formatted_request, sizeof(formatted_request), request_template, client->ip, strlen(message), message);
+  if (tcp_send(client->server, formatted_request, strlen(formatted_request), TIMEOUT)) {
+    printf("Message sent: %s\n", message);
+  } else {
+    fprintf(stderr, "Failed to send message\n");
+  }
 }
 
 void client_receive_message(game_client *client) {
-    int bytes_received = tcp_receive(client->server, client->buffer, sizeof(client->buffer), TIMEOUT);
-    if (bytes_received > 0) {
-        printf("Received: %.*s\n", bytes_received, client->buffer);
-    } else {
-        printf("No message received within timeout\n");
-    }
+  int bytes_received = tcp_receive(client->server, client->buffer, sizeof(client->buffer), 0);
+  if (bytes_received > 0) {
+    printf("Received: %.*s\n", bytes_received, client->buffer);
+  } 
+  else {
+    printf("No message received within timeout\n");
+  }
 }
+
--- src/networking/server.c
+++ src/networking/server.c
@@ -7,46 +7,47 @@
 static int MAX_CLIENTS = 10;
 
 void server_start(game_server *server, const char *address, const char *port, int max_clients) {
-    tcp_init();
-    server->server = tcp_open_server(address, port, max_clients);
-    if (server->server) {
-        printf("Server started at %s:%s\n", address, port);
-    } else {
-        fprintf(stderr, "Failed to start server at %s:%s\n", address, port);
-        exit(EXIT_FAILURE);
-    }
+  tcp_init();
+  server->server = tcp_open_server(address, port, max_clients);
+  if (server->server) {
+    printf("Server started at %s:%s\n", address, port);
+  } 
+  else {
+    fprintf(stderr, "Failed to start server at %s:%s\n", address, port);
+    exit(EXIT_FAILURE);
+  }
 }
 
 void server_run(game_server *server) {
-    printf("Waiting for clients to connect...\n");
-    while (true) {
-        tcp_channel *client_channel = tcp_accept(server->server, 0);
-        if (client_channel) {
-            game_server_client client = { client_channel, 0 }; // You could generate or assign an ID here
-            printf("Client connected!\n");
-            handle_client(&client);
-            tcp_close_channel(client_channel);
-        }
+  printf("Waiting for clients to connect...\n");
+  while (true) {
+    tcp_channel *client_channel = tcp_accept(server->server, 0);
+    if (client_channel) {
+      game_server_client client = { client_channel, 0 };
+      printf("Client connected!\n");
+      handle_client(&client);
+      tcp_close_channel(client_channel);
     }
+  }
 }
 
 void handle_client(game_server_client *client) {
-    char buffer[TCP_STREAM_BUFFER_SIZE];
-    while (true) {
-        int bytes_received = tcp_receive(client->channel, buffer, TCP_STREAM_BUFFER_SIZE, 1000);
-        if (bytes_received > 0) {
-            printf("Received: %.*s\n", bytes_received, buffer);
-            // Echo message back to client
-            tcp_send(client->channel, buffer, bytes_received, 1000);
-        } else {
-            printf("Client disconnected or timeout.\n");
-            break;
-        }
+  char buffer[TCP_STREAM_BUFFER_SIZE];
+  while (true) {
+    int bytes_received = tcp_receive(client->channel, buffer, TCP_STREAM_BUFFER_SIZE, 1000);
+    if (bytes_received > 0) {
+      printf("Received: %.*s\n", bytes_received, buffer);
+      tcp_send(client->channel, buffer, bytes_received, 1000);
+    } 
+    else {
+      printf("Client disconnected or timeout.\n");
+      break;
     }
+  }
 }
 
 void server_shutdown(game_server *server) {
-    tcp_close_server(server->server);
-    tcp_term();
-    printf("Server shut down.\n");
+  tcp_close_server(server->server);
+  tcp_term();
+  printf("Server shut down.\n");
 }
