7e6f60b8b7f04543c3d432e6f9dc1cce5e5261f0
KHodow677 <kylehodowany@utexas.edu>
2024-10-07 01:03:56 -0500

    Matchmaking Rooms

--- svr/server.js
+++ svr/server.js
@@ -1,39 +1,87 @@
 var net = require("net");
 var clients = [];
 var clientData = {};
-let client_id_counter = 16777215;
+var rooms = {};
 
-function get_client_id(socket) {
-  const client = clients.find(client => client.sock === socket);
-  return client ? client.id : undefined;
+function get_other_client_in_room(roomCode, socket) {
+  const room = rooms[roomCode];
+  return room ? room.clients.find(c => c.sock !== socket) : undefined;
 }
 
 var server = net.createServer(function(socket) {
-  const client_id = client_id_counter.toString(16).padStart(6, "0").toUpperCase();
-  client_id_counter--;
-  const client = { id: client_id, sock: socket };
+  const client = { sock: socket, room: null };
   clients.push(client);
-  console.log("Client " + client_id + " connected");
+  console.log("A client connected");
   socket.on("data", function(data) {
     const request = data.toString();
-    if (request.startsWith("POST /send")) {
+    if (request.startsWith("POST /room/create")) {
       const body = request.split("\r\n\r\n")[1];
       try {
         const parsed_data = JSON.parse(body);
-        const parsed_receiver = parsed_data.message.toString().split(":")[0];
-        parsed_data.message = parsed_data.message.toString().split(":")[1];
-        clients.forEach(function(client) {
-          if (client.id == parsed_receiver) {
-            client.sock.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n${parsed_data.message}`);
-          }
-        });
-        clientData = parsed_data;
-        socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n${parsed_data.message}`);
+        const roomCode = parsed_data.roomCode;
+        if (rooms[roomCode]) {
+          socket.write("HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\n\r\nRoom already exists");
+        } 
+        else {
+          rooms[roomCode] = { clients: [client] };
+          client.room = roomCode;
+          socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nRoom ${roomCode} created`);
+          console.log(`Room ${roomCode} created by a client`);
+        }
       } 
       catch (e) {
         console.error("Error parsing JSON:", e);
         socket.write("HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\n\r\nInvalid JSON");
       }
+    } 
+    else if (request.startsWith("POST /room/join")) {
+      const body = request.split("\r\n\r\n")[1];
+      try {
+        const parsed_data = JSON.parse(body);
+        const roomCode = parsed_data.roomCode;
+        if (rooms[roomCode]) {
+          const room = rooms[roomCode];
+          if (room.clients.length >= 2) {
+            socket.write("HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\n\r\nRoom is full");
+          } 
+          else {
+            room.clients.push(client);
+            client.room = roomCode;
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nJoined room ${roomCode}`);
+            console.log(`A client joined Room ${roomCode}`);
+          }
+        } 
+        else {
+          socket.write("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\nRoom not found");
+        }
+      } 
+      catch (e) {
+        console.error("Error parsing JSON:", e);
+        socket.write("HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\n\r\nInvalid JSON");
+      }
+    } 
+    else if (request.startsWith("POST /send")) {
+      const body = request.split("\r\n\r\n")[1];
+      try {
+        const parsed_data = JSON.parse(body);
+        if (client.room) {
+          const other_client = get_other_client_in_room(client.room, socket);
+          if (other_client) {
+            other_client.sock.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n${parsed_data.message}`);
+          } 
+          else {
+            socket.write("HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\n\r\nNo other client in the room");
+          }
+        } 
+        else {
+          socket.write("HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\n\r\nClient not in any room");
+        }
+      } 
+      catch (e) {
+        console.error("Error parsing JSON:", e);
+        socket.write("HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\n\r\nInvalid JSON");
+      }
+
     } 
     else if (request.startsWith("GET /receive")) {
       console.log("Client requested data.");
@@ -44,19 +92,17 @@ var server = net.createServer(function(socket) {
       socket.write("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\nServer not found");
     }
   });
-
   socket.on("end", function() {
-    console.log("Client " + get_client_id(socket) + " disconnected");
-    clients = clients.filter(client => client.sock !== socket);
-    client_id_counter++;
+    console.log("A client disconnected");
+    clients = clients.filter(c => c.sock !== socket);
   });
-
   socket.on("error", function(err) {
-    console.error("KHG Error: ", err);
-    clients = clients.filter(client => client.sock !== socket);
+    console.error("Error: ", err);
+    clients = clients.filter(c => c.sock !== socket);
   });
 });
 
 server.listen(3000, function() {
   console.log("HTTP server started on port 3000");
 });
+
