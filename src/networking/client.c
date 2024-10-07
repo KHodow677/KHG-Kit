87a779f6d2edf3a72a1b8fbb4e7b2c74d93d6a50
KHodow677 <kylehodowany@utexas.edu>
2024-10-07 02:16:07 -0500

    Server Update

--- src/networking/client.c
+++ src/networking/client.c
@@ -12,6 +12,8 @@ void hoster_run(const char *room_code) {
   tcp_channel *channel = tcp_connect("165.22.176.143", "http");
   tcp_client_create_room(channel, room_code);
   tcp_client_receive(channel);
+  tcp_close_channel(channel);
+  tcp_term();
 }
 
 void joiner_run(const char *room_code) {
@@ -21,12 +23,13 @@ void joiner_run(const char *room_code) {
   const char *message = "Hello from the sender!";
   tcp_client_send(channel, "send_message", message);
   tcp_client_receive(channel);
+  tcp_close_channel(channel);
+  tcp_term();
 }
 
 void tcp_client_create_room(tcp_channel *channel, const char *room_code) {
   char data[256];
   snprintf(data, sizeof(data), "{\"command\":\"create_room\", \"room_code\":\"%s\"}", room_code);
-  
   const char *request = "POST /send HTTP/1.1\r\nHost: 165.22.176.143\r\nContent-Type: application/json\r\nContent-Length: %zu\r\n\r\n%s";
   char formatted_request[1024];
   snprintf(formatted_request, sizeof(formatted_request), request, strlen(data), data);
@@ -50,8 +53,6 @@ void tcp_client_send(tcp_channel *channel, const char *command, const char *mess
   snprintf(formatted_request, sizeof(formatted_request), request, strlen(data), data);
   tcp_send(channel, formatted_request, strlen(formatted_request), 500);
   tcp_stream_receive(channel, ignore_buffer, NULL, 500);
-  tcp_close_channel(channel);
-  tcp_term();
 }
 
 void tcp_client_receive(tcp_channel *channel) {
@@ -61,7 +62,5 @@ void tcp_client_receive(tcp_channel *channel) {
   while (1) {
     tcp_stream_receive_no_timeout(channel, message_buffer, NULL);
   }
-  tcp_close_channel(channel);
-  tcp_term();
 }
 
--- svr/server.js
+++ svr/server.js
@@ -16,7 +16,6 @@ var server = net.createServer(function(socket) {
   console.log("Client " + client_id + " connected");
   socket.on("data", function(data) {
     const request = data.toString();
-    
     if (request.startsWith("POST /send")) {
       const body = request.split("\r\n\r\n")[1];
       try {
@@ -27,11 +26,18 @@ var server = net.createServer(function(socket) {
           const room_code = parsed_data.room_code;
           console.log(`Room ${room_code} created by client ${client_id}`);
           socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nRoom ${room_code} created.`);
-        } else if (command === "join_room") {
+        } 
+        else if (command === "join_room") {
           const room_code = parsed_data.room_code;
           console.log(`Client ${client_id} joined room ${room_code}`);
           socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nJoined room ${room_code}.`);
-        } else {
+        } 
+        else if (command === "send_message") {
+          const message = parsed_data.message;
+          console.log(`Client ${client_id} sent_message "${message}"`);
+          socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nSend message "${message}"`);
+        } 
+        else {
           const parsed_receiver = parsed_data.message.toString().split(":")[0];
           parsed_data.message = parsed_data.message.toString().split(":")[1];
           clients.forEach(function(client) {
