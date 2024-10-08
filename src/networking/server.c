ced79f08e918f9ef30c8a66de005ddbdb0257ee9
hodowany <Kyle.Hodowany@caemilusa.com>
2024-10-08 13:28:17 -0500

    Query

--- src/networking/server.c
+++ src/networking/server.c
@@ -1,13 +1,12 @@
 #include "networking/server.h"
 #include "khg_dbm/database.h"
+#include "khg_dbm/parser.h"
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
 #if defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
-#else 
-#include <linux/limits.h>
-#include <sys/stat.h>
 #endif
 
 dbm_database *PLAYER_DB = NULL;
@@ -15,12 +14,10 @@ dbm_database *PLAYER_DB = NULL;
 void server_run() {
   PLAYER_DB = dbm_create_database("player_db");
   load_db("player_db");
+  printf("CREATE TABLE\n");
+  dbm_interactive(&PLAYER_DB, "Create Table EmployeeDemographics (EmployeeID int, FirstName varchar(50), LastName varchar(50), Age int, Gender varchar(50))");
+  dbm_interactive(&PLAYER_DB, "Insert into EmployeeDemographics VALUES (1001, 'Jim', 'Halpert', 30, 'Male'), (1002, 'Pam', 'Beasley', 30, 'Female'), (1003, 'Dwight', 'Schrute', 29, 'Male'), (1004, 'Angela', 'Martin', 31, 'Female'), (1005, 'Toby', 'Flenderson', 32, 'Male'), (1006, 'Michael', 'Scott', 35, 'Male'), (1007, 'Meredith', 'Palmer', 32, 'Female'), (1008, 'Stanley', 'Hudson', 38, 'Male'), (1009, 'Kevin', 'Malone', 31, 'Male')");
   save_db("player_db");
-}
-
-int file_exists (char *filename) {
-  struct stat buffer;
-  return (stat(filename, &buffer) == 0);
 }
 
 void load_db(const char *asset_name) {
@@ -29,12 +26,15 @@ void load_db(const char *asset_name) {
   _getcwd(cwd, sizeof(cwd));
   size_t cwd_len = strlen(cwd);
   size_t asset_dir_len = cwd_len + strlen("\\res") + 1;
-  size_t path_len = asset_dir_len + strlen("\\assets\\fonts\\") + strlen(asset_name) + strlen(".") + strlen(file_extension) + 1;
+  size_t meta_path_len = asset_dir_len + strlen("\\assets\\db\\") + strlen(asset_name) + strlen(".meta") + 1;
+  size_t db_path_len = asset_dir_len + strlen("\\assets\\db\\") + strlen(asset_name) + strlen(".db") + 1;
   char asset_dir[asset_dir_len];
-  char path[path_len];
+  char meta_path[meta_path_len];
+  char db_path[db_path_len];
   snprintf(asset_dir, sizeof(asset_dir), "%s\\res", cwd);
-  snprintf(path, sizeof(path), "%s\\assets\\fonts\\%s.%s", asset_dir, asset_name, file_extension);
-  return gfx_load_font(path, font_size);
+  snprintf(meta_path, sizeof(meta_path), "%s\\assets\\db\\%s.%s", asset_dir, asset_name, "meta");
+  snprintf(db_path, sizeof(db_path), "%s\\assets\\db\\%s.%s", asset_dir, asset_name, "db");
+  dbm_read_files(&PLAYER_DB, meta_path, db_path);
 #else 
   char cwd[PATH_MAX];
   getcwd(cwd, sizeof(cwd));
@@ -48,9 +48,7 @@ void load_db(const char *asset_name) {
   snprintf(asset_dir, sizeof(asset_dir), "%s/res", cwd);
   snprintf(meta_path, sizeof(meta_path), "%s/assets/db/%s.%s", asset_dir, asset_name, "meta");
   snprintf(db_path, sizeof(db_path), "%s/assets/db/%s.%s", asset_dir, asset_name, "db");
-  if (file_exists(meta_path) && file_exists(db_path)) {
-    dbm_read_files(&PLAYER_DB, meta_path, db_path);
-  }
+  dbm_read_files(&PLAYER_DB, meta_path, db_path);
 #endif
 }
 
@@ -60,12 +58,15 @@ void save_db(const char *asset_name) {
   _getcwd(cwd, sizeof(cwd));
   size_t cwd_len = strlen(cwd);
   size_t asset_dir_len = cwd_len + strlen("\\res") + 1;
-  size_t path_len = asset_dir_len + strlen("\\assets\\fonts\\") + strlen(asset_name) + strlen(".") + strlen(file_extension) + 1;
+  size_t meta_path_len = asset_dir_len + strlen("\\assets\\db\\") + strlen(asset_name) + strlen(".meta") + 1;
+  size_t db_path_len = asset_dir_len + strlen("\\assets\\db\\") + strlen(asset_name) + strlen(".db") + 1;
   char asset_dir[asset_dir_len];
-  char path[path_len];
+  char meta_path[meta_path_len];
+  char db_path[db_path_len];
   snprintf(asset_dir, sizeof(asset_dir), "%s\\res", cwd);
-  snprintf(path, sizeof(path), "%s\\assets\\fonts\\%s.%s", asset_dir, asset_name, file_extension);
-  return gfx_load_font(path, font_size);
+  snprintf(meta_path, sizeof(meta_path), "%s\\assets\\db\\%s.%s", asset_dir, asset_name, "meta");
+  snprintf(db_path, sizeof(db_path), "%s\\assets\\db\\%s.%s", asset_dir, asset_name, "db");
+  dbm_write_files(&PLAYER_DB, meta_path, db_path);
 #else 
   char cwd[PATH_MAX];
   getcwd(cwd, sizeof(cwd));
@@ -82,4 +83,3 @@ void save_db(const char *asset_name) {
   dbm_write_files(&PLAYER_DB, meta_path, db_path);
 #endif
 }
-
