dc012c12d171eefffb9cb8585e471fe7d6d0c65e
KHodow677 <kylehodowany@utexas.edu>
2024-10-12 09:47:22 -0500

    Test

--- src/networking/client.c
+++ src/networking/client.c
@@ -38,21 +38,21 @@ void tcp_client_send(tcp_channel *channel) {
   tcp_init();
   const char *data = "Set some data";
   const char *request = "POST /send HTTP/1.1\r\nHost: 165.22.176.143\r\nContent-Type: application/json\r\nContent-Length: %zu\r\n\r\n%s";
-  char formatted_request[255];
+  char formatted_request[1024];
   snprintf(formatted_request, sizeof(formatted_request), request, strlen(data), data);
   tcp_send(channel, formatted_request, strlen(formatted_request), 500);
-  char formatted_response[255];
-  tcp_receive(channel, formatted_response, 255, 500);
+  char formatted_response[1024];
+  tcp_receive(channel, formatted_response, 1024, 500);
 }
 
 void tcp_client_receive(tcp_channel *channel) {
   tcp_init();
   const char *data = "Get some data";
   const char *request = "GET /receive HTTP/1.1\r\nHost: 165.22.176.143\r\n\r\n";
-  char formatted_request[255];
+  char formatted_request[1024];
   snprintf(formatted_request, sizeof(formatted_request), request, strlen(data), data);
   tcp_send(channel, formatted_request, strlen(formatted_request), 500);
-  char formatted_response[255];
-  tcp_receive(channel, formatted_response, 255, 500);
+  char formatted_response[1024];
+  tcp_receive(channel, formatted_response, 1024, 500);
 }
 
