54fbf860e0ef91d5cadb4c0e3fd2f29839bbcba0
KHodow677 <kylehodowany@utexas.edu>
2024-10-19 12:15:33 -0500

    Env

--- src/resources/texture_loader.c
+++ src/resources/texture_loader.c
@@ -27,11 +27,11 @@ gfx_texture *get_or_add_texture(int tex_id) {
 
 void generate_textures() {
   TEXTURE_ASSET_REF[SQUARE] = (texture_asset) { (gfx_texture){ 0 }, false, "square", "png", 256, 256};
-  TEXTURE_ASSET_REF[MAIN_ENVIRONMENT_GROUND] = (texture_asset) { (gfx_texture){ 0 }, false, "main/environment/ground_layer", "png", 1920, 906};
-  TEXTURE_ASSET_REF[MAIN_ENVIRONMENT_TREE_0] = (texture_asset) { (gfx_texture){ 0 }, false, "main/environment/tree_layer_0", "png", 1920, 441};
-  TEXTURE_ASSET_REF[MAIN_ENVIRONMENT_TREE_1] = (texture_asset) { (gfx_texture){ 0 }, false, "main/environment/tree_layer_1", "png", 1920, 612};
-  TEXTURE_ASSET_REF[MAIN_ENVIRONMENT_MOUNTAIN_0] = (texture_asset) { (gfx_texture){ 0 }, false, "main/environment/mountain_layer_0", "png", 1920, 582};
-  TEXTURE_ASSET_REF[MAIN_ENVIRONMENT_MOUNTAIN_1] = (texture_asset) { (gfx_texture){ 0 }, false, "main/environment/mountain_layer_1", "png", 1920, 567};
-  TEXTURE_ASSET_REF[MAIN_ENVIRONMENT_BACKGROUND] = (texture_asset) { (gfx_texture){ 0 }, false, "main/environment/background_layer", "png", 1920, 1080};
+  TEXTURE_ASSET_REF[MAIN_ENVIRONMENT_GROUND] = (texture_asset) { (gfx_texture){ 0 }, false, "main/environment/ground_layer", "png", 1282, 722 };
+  TEXTURE_ASSET_REF[MAIN_ENVIRONMENT_TREE_0] = (texture_asset) { (gfx_texture){ 0 }, false, "main/environment/tree_layer_00", "png", 1282, 722 };
+  TEXTURE_ASSET_REF[MAIN_ENVIRONMENT_TREE_1] = (texture_asset) { (gfx_texture){ 0 }, false, "main/environment/tree_layer_01", "png", 1282, 722 };
+  TEXTURE_ASSET_REF[MAIN_ENVIRONMENT_MOUNTAIN_0] = (texture_asset) { (gfx_texture){ 0 }, false, "main/environment/mountain_layer_00", "png", 1282, 722 };
+  TEXTURE_ASSET_REF[MAIN_ENVIRONMENT_MOUNTAIN_1] = (texture_asset) { (gfx_texture){ 0 }, false, "main/environment/mountain_layer_01", "png", 1282, 722 };
+  TEXTURE_ASSET_REF[MAIN_ENVIRONMENT_BACKGROUND] = (texture_asset) { (gfx_texture){ 0 }, false, "main/environment/background_layer", "png", 1282, 722 };
 }
 
