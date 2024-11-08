59c952fc974782dc431ad1e86c46dcea4883e21a
KHodow677 <kylehodowany@utexas.edu>
2024-11-08 12:36:58 -0600

    Animation Support

---
+++ engine/khg_ani/include/khg_ani/spine.h
@@ -0,0 +1,63 @@
+/******************************************************************************
+ * Spine Runtimes License Agreement
+ * Last updated July 28, 2023. Replaces all prior versions.
+ *
+ * Copyright (c) 2013-2023, Esoteric Software LLC
+ *
+ * Integration of the Spine Runtimes into software or otherwise creating
+ * derivative works of the Spine Runtimes is permitted under the terms and
+ * conditions of Section 2 of the Spine Editor License Agreement:
+ * http://esotericsoftware.com/spine-editor-license
+ *
+ * Otherwise, it is permitted to integrate the Spine Runtimes into software or
+ * otherwise create derivative works of the Spine Runtimes (collectively,
+ * "Products"), provided that each user of the Products must obtain their own
+ * Spine Editor license and redistribution of the Products in any form must
+ * include this license and copyright notice.
+ *
+ * THE SPINE RUNTIMES ARE PROVIDED BY ESOTERIC SOFTWARE LLC "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ESOTERIC SOFTWARE LLC BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES,
+ * BUSINESS INTERRUPTION, OR LOSS OF USE, DATA, OR PROFITS) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THE
+ * SPINE RUNTIMES, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *****************************************************************************/
+
+#ifndef SPINE_SPINE_H_
+#define SPINE_SPINE_H_
+
+#include "khg_ani/dll.h"
+#include "khg_ani/Array.h"
+#include "khg_ani/Animation.h"
+#include "khg_ani/AnimationState.h"
+#include "khg_ani/AnimationStateData.h"
+#include "khg_ani/Atlas.h"
+#include "khg_ani/AtlasAttachmentLoader.h"
+#include "khg_ani/Attachment.h"
+#include "khg_ani/AttachmentLoader.h"
+#include "khg_ani/Bone.h"
+#include "khg_ani/BoneData.h"
+#include "khg_ani/RegionAttachment.h"
+#include "khg_ani/VertexAttachment.h"
+#include "khg_ani/MeshAttachment.h"
+#include "khg_ani/BoundingBoxAttachment.h"
+#include "khg_ani/ClippingAttachment.h"
+#include "khg_ani/Physics.h"
+#include "khg_ani/PointAttachment.h"
+#include "khg_ani/Skeleton.h"
+#include "khg_ani/SkeletonBounds.h"
+#include "khg_ani/SkeletonData.h"
+#include "khg_ani/SkeletonBinary.h"
+#include "khg_ani/SkeletonJson.h"
+#include "khg_ani/Skin.h"
+#include "khg_ani/Slot.h"
+#include "khg_ani/SlotData.h"
+#include "khg_ani/SkeletonClipping.h"
+#include "khg_ani/Event.h"
+#include "khg_ani/EventData.h"
+
+#endif /* SPINE_SPINE_H_ */
