30be52b2b1f79d5f03fea3bace6e520cf8c1f290
KHodow677 <kylehodowany@utexas.edu>
2024-12-08 22:03:12 -0600

    Tilemap Loading

---
+++ include/resources/tile_loader.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void generate_tile_from_file(tile *r, const char *filepath, const char *rig_section);
+
--- src/scene/scenes/main/player_builder.c
+++ src/scene/scenes/main/player_builder.c
@@ -18,9 +18,9 @@ void build_player(const float x, const float y, const int render_layer) {
   comp_physics_constructor_info comp_physics_ci = { PHYSICS_BOX, 300.0f, 256.0f, 1.0f, phy_vector2_new(x, y), 0.0f, true, true };
   comp_physics *cp = sys_physics_add(entity, &comp_physics_ci);
   comp_renderer_constructor_info comp_renderer_ci = { cp->body, PLAYER_BODY, render_layer, 1.0f, false };
-  comp_renderer *cr = sys_renderer_add(entity, &comp_renderer_ci, generate_rig_builder_from_file("res/assets/anim/rigs/player.ini", "player_root", 1));
-  generate_rig_from_file(&cr->rig, "res/assets/anim/rigs/player.ini", "player_bones");
-  generate_animation_from_path(&cr->rig, "res/assets/anim/frames/player/idle/", "player_frame", 0, 8);
+  comp_renderer *cr = sys_renderer_add(entity, &comp_renderer_ci, generate_rig_builder_from_file("res/assets/data/anim/rigs/player.ini", "player_root", 1));
+  generate_rig_from_file(&cr->rig, "res/assets/data/anim/rigs/player.ini", "player_bones");
+  generate_animation_from_path(&cr->rig, "res/assets/data/anim/frames/player/idle/", "player_frame", 0, 8);
   set_state_and_frame(&cr->rig, 0, 0);
   comp_light_constructor_info comp_light_ci = { cp->body, (light){ (vec2s){ 0.0, 0.0 }, 250.0f }, phy_vector2_new(0.0f, 0.0f) };
   comp_light *cl = sys_light_add(entity, &comp_light_ci);
