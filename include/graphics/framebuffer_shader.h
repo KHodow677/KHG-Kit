f24e2a31447b0f54052008d2218c7fbb768f4be7
KHodow677 <kylehodowany@utexas.edu>
2024-12-13 22:57:39 -0600

    Bloom

--- include/graphics/framebuffer_shader.h
+++ include/graphics/framebuffer_shader.h
@@ -53,29 +53,3 @@ const char *framebuffer_frag_src =
   "  frag_color = clamp(noisyColor, 0.0, 1.0);\n"
   "}\n";
 
-/*const char *framebuffer_frag_src = */
-/*  "#version 450 core\n"*/
-/*  "out vec4 frag_color;\n"*/
-/*  "in vec2 TexCoord;\n"*/
-/*  "uniform sampler2D u_framebuffer_texture;\n"*/
-/*  "uniform float time;\n"*/
-/*  "uniform float brightness_decrease;\n"*/
-/*  "uniform float noise_intensity;\n"*/
-/*  "uniform float distortion_strength;\n"*/
-/*  "float random(vec2 uv) {\n"*/
-/*  "  return fract(sin(dot(uv.xy, vec2(12.9898, 78.233))) * 43758.5453);\n"*/
-/*  "}\n"*/
-/*  "vec4 apply_noise(vec4 color, vec2 uv) {\n"*/
-/*  "  float noise = random(uv * time) * noise_intensity;\n"*/
-/*  "  return vec4(color.rgb + noise, color.a);\n"*/
-/*  "}\n"*/
-/*  "void main() {\n"*/
-/*  "  vec4 originalColor = texture(u_framebuffer_texture, TexCoord);\n"*/
-/*  "  vec2 distortedTexCoord = TexCoord + vec2(sin(TexCoord.y * 10.0 + time) * distortion_strength, 0.0);\n"*/
-/*  "  distortedTexCoord = clamp(distortedTexCoord, vec2(0.0), vec2(1.0));\n"*/
-/*  "  vec4 distortedColor = texture(u_framebuffer_texture, distortedTexCoord);\n"*/
-/*  "  distortedColor.rgb = distortedColor.rgb - brightness_decrease;\n"*/
-/*  "  distortedColor = apply_noise(distortedColor, distortedTexCoord);\n"*/
-/*  "  frag_color = distortedColor;\n"*/
-/*  "}\n";*/
-/**/
