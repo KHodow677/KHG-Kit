495e41ecab954b0ecd75652e6524cb19a879225d
KHodow677 <kylehodowany@utexas.edu>
2024-12-29 07:51:20 -0600

    DBM Update Needs Testing

--- common/engine/khg_dbm/include/khg_dbm/btree.h
+++ common/engine/khg_dbm/include/khg_dbm/btree.h
@@ -4,21 +4,22 @@
 #include <stdbool.h>
 
 typedef struct btree_node {
-  int64_t key;
-  int64_t value;
+  long key;
+  long value;
   struct btree_node *left;
   struct btree_node *right;
 } dbm_btree_node;
 
 typedef struct dbm_btree {
-  int64_t size;
+  long size;
   dbm_btree_node *root;
 } dbm_btree;
 
 void btree_init(dbm_btree *tree);
 void btree_destroy(dbm_btree *tree);
 
-bool btree_contains(const dbm_btree *tree, int64_t key);
-dbm_btree_node *btree_search(const dbm_btree *tree, int64_t key);
-dbm_btree_node *btree_insert(dbm_btree *tree, int64_t key, int64_t value);
-bool btree_remove(dbm_btree *tree, int64_t key, int64_t *old_value);
+bool btree_contains(const dbm_btree *tree, long key);
+dbm_btree_node *btree_search(const dbm_btree *tree, long key);
+dbm_btree_node *btree_insert(dbm_btree *tree, long key, long value);
+bool btree_remove(dbm_btree *tree, long key, long *old_value);
+
