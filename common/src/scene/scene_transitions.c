8dfa5829c179bed1fcaa243f5ee31ce4b4a0746b
KHodow677 <kylehodowany@utexas.edu>
2025-01-09 21:54:17 -0600

    Res Additions

--- common/src/scene/scene_transitions.c
+++ common/src/scene/scene_transitions.c
@@ -3,9 +3,6 @@
 #include "camera/camera.h"
 #include "ecs/ecs_manager.h"
 #include "physics/physics.h"
-#include "resources/area_loader.h"
-#include "scene/scenes/main/area_builder.h"
-#include "scene/scenes/main/player_builder.h"
 #include "khg_phy/core/phy_vector.h"
 #include "khg_stm/state_machine.h"
 
@@ -15,11 +12,6 @@ void load_main_scene(void *old_state_data, stm_event *event, void *new_state_dat
   camera_setup(&CAMERA);
   setup_lights_texture();
   setup_lights_shader();
-  build_area(DUNGEON_BG, 7, true);
-  build_area(DUNGEON_0, 8, false);
-  /*build_player(256, 726, 9);*/
-  build_player(256, 826, 9);
-  /*build_zone(PLAYER_INFO.physics->body, 950, 1150);*/
 };
 
 void load_game_scene(void *old_state_data, stm_event *event, void *new_state_data) {
